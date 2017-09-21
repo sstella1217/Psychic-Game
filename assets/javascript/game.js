8be3bad1ebf30f243611f1b1da37e0c66ae8db2e
Stephanie Wertz <stephaniewertz@Stephanies-MacBook-Air.local>
2017-09-20 21:06:36 -0500

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,42 +5,60 @@ var compLetter = ['a','b','c','d','e','f','g',
 				  'w','x','y','z'];
 
 var wins = 0;
-var losses = 0;				  
+var losses = 0;
+var moves = 10;	
+
+
+
+ 
 
  document.onkeyup = function(event) {
+
+  if (moves > 0) {
+      // User key pressed
+      var userGuess = event.key;
+
 
       // Computers Letter
       var computerGuess = compLetter[Math.floor(Math.random() * compLetter.length)];
 
-      // User key pressed
-      var userGuess = event.key;
-
-      alert(userGuess);
-      alert(computerGuess);
-
-      if (losses === '10'){
-      	 alert("Game Over");
-      }
+      
+      document.getElementById("userGuessElement").innerHTML = userGuess;
+      //document.getElementById("computerGuessElement").innerHTML = computerGuess;
+   
+      //alert(userGuess);
+      //alert(computerGuess);    
+      	      
 
       if (userGuess === computerGuess){
       	wins ++
+        moves --
       	alert("You Got it Right");
-      	
+            	
       }
+      
       else {
       	losses ++
-      	alert("try again");
-      	
-      	}
+      	moves --
+            alert("try again");
+	    }
       
+       
+       document.getElementById("winsElement").innerHTML = wins;
+       document.getElementById("lossesElement").innerHTML = losses; 
+       document.getElementById("movesElement").innerHTML = moves;     
+       
+      }
+      else {
+      console.log("Game Over");
+      }
+  };    
       
-    
+
       //document.getElementById("userGuessElement").innerHTML = userGuess;
 	  //document.getElementById("computerGuessElement").innerHTML = computerGuess;
-	  document.getElementById("winsElement").innerHTML = wins;
-	  document.getElementById("lossesElement").innerHTML = losses;
-     
-    };
+	 
     
+
 
 
